916cb8481be1d5ead8744f9bffea89cd7b78d2b9
Moïse Gui <k.moise.gui@gmail.com>
2021-12-29 10:43:02 +0100

    last commit is deployed

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,12 +1,10 @@
-import 'dart:ffi';
-
 import 'package:json_annotation/json_annotation.dart';
 
+part 'category.dart';
 part 'choice.dart';
 part 'course.dart';
 part 'data_dummy.dart';
-part 'quiz.dart';
-part 'user.dart';
-part 'category.dart';
-part 'reponse.dart';
 part 'models.g.dart';
+part 'quiz.dart';
+part 'reponse.dart';
+part 'user.dart';
